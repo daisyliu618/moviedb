969884ba8d7e5d29b2ec99456785c231ab8f2ae2
Daisy <daisyliudesign@gmail.com>
2021-02-22 12:44:14 -0500

    commit

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -204,6 +204,7 @@ export const Home = () => {
             of using Lorem Ipsum is that it has a more-or-less normal
             distribution of letters, as opposed to using 'Content here, content
           </p> */}
+          
           <ul className='list-inline'>
             <li className='list-inline-item'>
               <a href='/' style={{ color: '#f4c10f' }}>
